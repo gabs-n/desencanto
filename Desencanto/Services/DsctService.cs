1484797536dc527b0e06f4edd2db966cc941e77d
gabi <nicoletigabriela2@gmail.com>
2023-03-24 11:01:34 -0300

    Alterações no DsctService.cs e no Program.cs pag 5 do PDF

--- Desencanto/Services/DsctService.cs
+++ Desencanto/Services/DsctService.cs
@@ -1,5 +1,4 @@
-
-using.System.Text.Json;
+using System.Text.Json;
 using Desencanto.Models;
 
 namespace Desencanto.Services
@@ -19,7 +18,70 @@ namespace Desencanto.Services
         public List<Personagem> GetPersonagens()
         {
             PopularSessao();
+            var personagens = JsonSerializer.Deserialize<List<Personagem>>
+            (_session.HttpContext.Session.GetString("Pokemons"));
+            return personagens;
+        }
+        public List<Tipo> GetTipos()
+        {
+            PopularSessao();
+            var tipos = JsonSerializer.Deserialize<List<Tipo>>
+            (_session.HttpContext.Session.GetString("Tipos"));
+            return tipos;
+        }
+        public Personagem GetPersonagem(int Numero)
+        {
+            var personagens = GetPersonagem();
+            return personagens.Where(p => p.Numero == Numero).FirstOrDefault();
+        }
+        public DesencantoDto GetDesencantoDto()
+        {
+            var persons = new DesencantoDto()
+            {
+                Personagens = GetPersonagens(),
+                Tipos = GetTipos()
+            };
+            return persons;
+        }
+        public DetailsDto GetDetailedPersonagem(int Numero)
+        {
+            var personagens = GetPersonagens();
+            var person = new DetailsDto()
+            {
+                Current = personagens.Where(p => p.Numero == Numero)
+            .FirstOrDefault(),
+                Prior = personagens.OrderByDescending(p => p.Numero)
+            .FirstOrDefault(p => p.Numero < Numero),
+                Next = personagens.OrderBy(p => p.Numero)
+            .FirstOrDefault(p => p.Numero > Numero),
+            };
+            return person;
+        }
+        public Tipo GetTipo(string Nome)
+        {
+            var tipos = GetTipos();
+            return tipos.Where(t => t.Nome == Nome).FirstOrDefault();
+        }
+        private void PopularSessao()
+        {
+            if (string.IsNullOrEmpty(_session.HttpContext.Session.GetString("Tipos")))
+            {
+                _session.HttpContext.Session
+                .SetString("Personagens", LerArquivo(personagemFile));
+                _session.HttpContext.Session
+                .SetString("Tipos", LerArquivo(tiposFile));
+            }
+        }
+        private string LerArquivo(string fileName)
+        {
+            using (StreamReader leitor = new StreamReader(fileName))
+            {
+                string dados = leitor.ReadToEnd();
+                return dados;
+            }
+        }
+    }
 
-        } 
+} 
     }
 }
